42481fb6ba148093533c11abbe1fa17dd2dfad90
liamrad <liamradhere@outlook.com>
2022-12-07 20:34:50 +0800

    chore: adjust compnent

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,10 @@
+<script setup lang="ts">
+const appConfig = useAppConfig()
+</script>
+
 <template>
   <div data-layout="default" p-8>
-    <GithubCorner />
+    <GithubCorner :repo="appConfig.repo" />
     <slot />
   </div>
 </template>
