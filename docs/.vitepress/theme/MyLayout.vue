6a3391e274556341151b21ad105b78515327b945
Byron <byronogis@outlook.com>
2023-01-04 19:36:08 +0800

    chore: define slot to show page info

---
+++ docs/.vitepress/theme/MyLayout.vue
@@ -0,0 +1,25 @@
+<script setup>
+import DefaultTheme from 'vitepress/theme'
+import { useData } from 'vitepress'
+import { computed } from 'vue'
+
+const { Layout } = DefaultTheme
+
+const frontmatter = useData().frontmatter
+const pkgUrl = computed(() => `https://www.npmjs.com/package/${frontmatter.value.title}`)
+</script>
+
+<template>
+  <Layout>
+    <template #doc-before>
+      <div class="vp-doc">
+        <h1>
+          {{ frontmatter.title }}
+        </h1>
+        <blockquote>{{ frontmatter.description }}</blockquote>
+        <blockquote><a :href="pkgUrl" target="_blank">在 npm 上查看</a></blockquote>
+      </div>
+
+    </template>
+  </Layout>
+</template>
