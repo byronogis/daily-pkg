935d8d84a43eede3de6c3c916857e1a83fdfc869
Byron <byronogis@outlook.com>
2023-07-09 18:05:12 +0800

    chore: adjust config follow new version
    vitepress@1.0.0-beta.5

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,33 +1,60 @@
+/**
+ * See https://vitepress.dev/reference/site-config
+ */
+
 import { defineConfig } from 'vitepress'
 import { sidebarArr } from './sidebar'
 
 export default defineConfig({
-  // See https://vitepress.vuejs.org/config/app-configs
-  appearance: true, // boolean | 'dark'
-  base: '/',
+  /**
+   * Site Metadata
+   */
+  title: 'Daily-Pkg',
+  titleTemplate: false,
   description: 'Learn about a package per day.',
   head: [],
-  ignoreDeadLinks: false,
   lang: 'zh-CN',
-  lastUpdated: true,
-  markdown: {
-    lineNumbers: true,
-  },
+  base: '/', // It should always start and end with a slash.
+
+  /**
+   * Routing
+   */
+  cleanUrls: false,
+  rewrites: {},
+
+  /**
+   * Build
+   * **relative to project root**
+   */
+  srcDir: '.',
+  srcExclude: undefined,
   outDir: './.vitepress/dist',
   cacheDir: './.vitepress/cache',
-  srcDir: '.',
-  title: 'Daily-Pkg',
-  titleTemplate: undefined,
-  cleanUrls: 'disabled', // Experimental
-  // See https://vitepress.vuejs.org/config/theme-configs
+  ignoreDeadLinks: false,
+  mpa: false, // experimental
+
+  /**
+   * Theming
+   */
+  appearance: true, // boolean | 'dark'
+  lastUpdated: true,
+
+  /**
+   * Default Theme Config
+   * See https://vitepress.dev/reference/default-theme-config
+   */
   themeConfig: {
+    i18nRouting: false,
     // logo: '',
     // siteTitle: false,
     // nav: [],
     sidebar: sidebarArr,
+    aside: true,
     outline: [2, 6],
-    // outlineTitle: '大纲',
-    socialLinks: [],
+    outlineTitle: '目录',
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/byronogis' },
+    ],
     footer: {
       message: 'Learn about a package per day.',
       copyright: 'Copyright @ PRESENT Byron',
@@ -36,11 +63,50 @@ export default defineConfig({
       pattern: 'https://github.com/byronogis/daily-pkg/edit/main/docs/:path',
       text: '在 GitHub 上编辑',
     },
-    lastUpdatedText: '最后更新',
+    lastUpdated: {
+      text: '最后更新',
+      formatOptions: {
+        dateStyle: 'short',
+        timeStyle: 'short',
+      },
+    },
+    // search: {
+    //   provider: 'algolia', // 'algolia' | 'local'
+    //   options: {},
+    // },
     // carbonAds: {},
     docFooter: {
       prev: '上一篇',
       next: '下一篇',
     },
+    darkModeSwitchLabel: '主题',
+    sidebarMenuLabel: '菜单',
+    returnToTopLabel: '返回顶部',
+    langMenuLabel: '语言',
+    externalLinkIcon: true,
+  },
+
+  /**
+   * markdown
+   * See https://vitepress.dev/reference/site-config#markdown
+   */
+  markdown: {
+    lineNumbers: true,
+  },
+
+  /**
+   * vite
+   * See https://vitepress.dev/reference/site-config#vite
+   */
+  vite: {
+    // Vite config options
+  },
+
+  /**
+   * vue
+   * See https://vitepress.dev/reference/site-config#vue
+   */
+  vue: {
+    // @vitejs/plugin-vue options
   },
 })
